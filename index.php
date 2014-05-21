c8804a2b3e74e6a470889b41d41cb657ce5b3a02
Bryce Howitson <graphixboy@gmail.com>
2014-05-21 15:14:10 -0500

    Remove some extra JS stuff that shouldn't be in the template

--- index.php
+++ index.php
@@ -51,12 +51,10 @@
 	
 	<!-- asynchronous google analytics change the UA-XXXXX-X to be your site's ID -->
 	<script>
-	if (scriptWrap.inProduction()) {
 		var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
 	    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
 	    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 	    s.parentNode.insertBefore(g,s)}(document,'script'));
-	}
 	</script>
 	
   
